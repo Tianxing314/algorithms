ea9d96b7ab985d1a612176d6310515c6a4386364
Tianxing Li <tianxing@Tianxings-MacBook-Pro-2.local>
2020-03-07 18:29:06 -0500

    optimize longest-palindrome-in-string.py

--- Strings/longest-palindrome-in-string.py
+++ Strings/longest-palindrome-in-string.py
@@ -1,68 +1,58 @@
 #https://practice.geeksforgeeks.org/problems/longest-palindrome-in-a-string/0
 #code
+def is_palindrome(i, j, string):
+    if (i <= j):
+        if table[i][j] == 1:
+            return True
+        elif table[i][j] == 0:
+            return False
+        elif i == j:
+            table[i][j] = 1
+            return True
+        elif i == j - 1:
+            if string[i] == string[j]:
+                table[i][j] = 1
+                return True
+            table[i][j] = 0
+            return False
+        elif string[i] != string[j]:
+            table[i][j] = 0
+            return False
+        elif string[i] == string[j]:
+            if is_palindrome(i+1, j-1, string):
+                table[i][j] = 1
+                return True
+            table[i][j] = 0
+            return False
+    else: #i > j:
+        print("i cannot be greater than j")
+        return False
+
 def longest_palindrome(string):
-    longest_str = ''
-    longest_len = 0
-    
-    length = len(string)
-    is_odd = (True if length%2 == 1 else False)
-    
-    if length == 0:
-        print('')
-        return 
-    
-    #odd_palindrome
-    for i in range(0, length):
-        odd_palindrome = string[i]
-        l = i - 1
-        r = i + 1
-        while(l >= 0 and r < length):
-            if string[l] == string[r]:
-                odd_palindrome = string[l] + odd_palindrome + string[r]
-            else: 
-                break
-            l-=1
-            r+=1
-        if (len(odd_palindrome) > longest_len):
-            longest_len = len(odd_palindrome)
-            longest_str = odd_palindrome
-    
-    #even_palindrome
-    for j in range(0, length):
-        even_palindrome = ''
-        l = j
-        r = j + 1
-        while(l >= 0 and r < length):
-            if string[l] == string[r]:
+    str_len = len(string)
+    for palindrome_len in range(str_len, 0, -1):
+        if palindrome_len == 1:
+            return string[0]
+        for i in range(0, str_len - palindrome_len + 1):
+            if (is_palindrome(i, i+palindrome_len-1, string)):
+                return string[i:i+palindrome_len]
 
-                even_palindrome = string[l] + even_palindrome + string[r]
-            else: 
-                break
-            l-=1
-            r+=1
-        if (len(even_palindrome) > longest_len):
-            longest_len = len(even_palindrome)
-            longest_str = even_palindrome
-    
-    if longest_len == 0: #return the first char if there is no palindrome string with length larger than 1
-        longest_str = string[0]
-    print(longest_str)
-    return
-        
 def read_input():
-    T = int(input())
-    testcases = []
-    case_num = 0
-   
-    while (case_num+1) <= T:
-        string = input()
-        testcases.append(string)
-        case_num += 1
-    return T, testcases 
+ T = int(input())
+ testcases = []
+ case_num = 0
 
-input_res = read_input() 
-T = input_res[0] 
-testcases = input_res[1] 
+ while (case_num+1) <= T:
+     string = input()
+     testcases.append(string)
+     case_num += 1
+ return T, testcases
+
+input_res = read_input()
+T = input_res[0]
+testcases = input_res[1]
 for i in range(0, T, 1):
     string = testcases[i]
-    longest_palindrome(string)
+    N = len(string)
+    table = [[2 for i in range(N)] for j in range(N)]
+    print(longest_palindrome(string))
