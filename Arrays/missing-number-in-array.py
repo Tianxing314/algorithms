2dcb2dc1315781f969f95e17a29a7a821f771b49
root <root@tl-0903-centos.devqacloud.com>
2020-03-09 16:33:36 -0500

    formatting for files in Arrays repo

--- Arrays/missing-number-in-array.py
+++ Arrays/missing-number-in-array.py
@@ -22,13 +22,14 @@ def read_input():
         case_num += 1
     return T, testcases 
 
-input_res = read_input() 
-T = input_res[0] 
-testcases = input_res[1] 
-for i in range(0, T, 1):
-    case = testcases[i]
-    N = case[0]
-    arr = case[1]
-    for i in range(0, N-1, 1):
-        arr[i] = int(arr[i])
-    missing_number_in_array(N, arr)
+if __name__=='__main__':
+    input_res = read_input() 
+    T = input_res[0] 
+    testcases = input_res[1] 
+    for i in range(0, T, 1):
+        case = testcases[i]
+        N = case[0]
+        arr = case[1]
+        for i in range(0, N-1, 1):
+            arr[i] = int(arr[i])
+        missing_number_in_array(N, arr)
--- Arrays/subarray-with-given-sum.py
+++ Arrays/subarray-with-given-sum.py
@@ -36,17 +36,16 @@ def read_input():
         case_num += 1
     return T, testcases
 
-input_res = read_input()
+if __name__=='__main__':
+    input_res = read_input()
+    T = input_res[0]
+    testcases = input_res[1]
 
-T = input_res[0]
-testcases = input_res[1]
-
-
-for i in range(0, T, 1):
-    case = testcases[i]
-    N = case[0]
-    S = case[1]
-    arr = case[2]
-    for i in range(0, N, 1):
-        arr[i] = int(arr[i])
-    sub_arr(N, S, arr)
+    for i in range(0, T, 1):
+        case = testcases[i]
+        N = case[0]
+        S = case[1]
+        arr = case[2]
+        for i in range(0, N, 1):
+            arr[i] = int(arr[i])
+        sub_arr(N, S, arr)
