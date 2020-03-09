22750a20f481f7727ee67477642909012160eae1
root <root@tl-0903-centos.devqacloud.com>
2020-03-09 15:48:52 -0500

    reverse a linked-list

---
+++ LinkedList/reverse-a-linked-list.py
@@ -0,0 +1,65 @@
+#https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+# use self.head to access head in the method
+def reverseList(self):
+    head = self.head
+    if self.head is None:
+        return None
+    prev_node = None
+    while(head is not None):
+        next_node = head.next
+        head.next = prev_node
+        prev_node = head
+        head = next_node
+    self.lastNode = self.head
+    self.head = prev_node
+    
+    
+
+#{ 
+#  Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+        
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.lastNode = None
+
+    def insert(self, val):
+        if self.head == None:
+            self.head = node(val)
+            self.lastNode = self.head
+        else:
+            new_node = node(val)
+            self.lastNode.next = new_node
+            self.lastNode = new_node
+
+    def createList(self, arr, n):
+        for i in range(n):
+            self.insert(arr[i])
+        return self.head
+
+    reverse_List = reverseList
+
+    def printList(self):
+        tmp = self.head
+        while tmp is not None:
+            print(tmp.data, end=" ")
+            tmp=tmp.next
+        print()
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        lis = Linked_List()
+        head = lis.createList(arr, n)
+        lis.reverse_List()
+        lis.printList()
+
+# } Driver Code Ends
